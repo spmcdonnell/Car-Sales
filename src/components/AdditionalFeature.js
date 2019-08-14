889bf9b42d4eb454ae817c06cdb516d3975c30f8
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-08-14 16:04:12 -0400

    Implement redux functionality and functionality for adding a car feature

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { addItem } from '../actions';
 
 const AdditionalFeature = props => {
-  return (
-    <li>
-      {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
-      {props.feature.name} (+{props.feature.price})
-    </li>
-  );
+    const { feature } = props;
+
+    return (
+        <li>
+            {/* Add an onClick that will let you add a feature to your car */}
+            <button onClick={() => props.addItem(feature)} className="button">
+                Add
+            </button>
+            {feature.name} (+{feature.price})
+        </li>
+    );
 };
 
-export default AdditionalFeature;
+const mapStateToProps = state => {
+    return state;
+};
+
+export default connect(
+    mapStateToProps,
+    { addItem }
+)(AdditionalFeature);
